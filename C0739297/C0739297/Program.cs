b4e20e1834aa09f3e00dce5890b782d8c8761e1a
Inderpreet Kaur <739297@student.tor.loc>
2019-07-31 11:01:16 -0400

    10:50 commit

--- C0739297/C0739297/Program.cs
+++ C0739297/C0739297/Program.cs
@@ -10,114 +10,88 @@ namespace C0739297
     {
         static void Main(string[] args)
         {
-            new Gematria().Run();
+            Gematrio run = new Gematrio();
+            run.Run();
         }
     }
-    class Gematria
+
+    class Gematrio
     {
-        public static Dictionary<string, int> LetterValue = new Dictionary<string, int>()
-{
-{"A",1},
-{"B",2 },
-{"C",3},
-{"D",4},
-{"E",5},
-{"F",6},
-{"G",7},
-{"H",8 },
-{"I",9},
-{"J",1},
-{"K",2 },
-{"L",3},
-{"M",4},
-{"N",5},
-{"O",6},
-{"P",7 },
-{"Q",8},
-{"R",9},
-{"S",1 },
-{"T",2},
-{"U",3 },
-{"V",4},
-{"W",5},
-{"X",6},
-{"Y",7},
-{"Z",8}
-
-
-};
-        String name;
+        string name;
         public void Run()
         {
-
-            Console.WriteLine(" I will tell you your future! -- What is your Name?");
+            Console.WriteLine("I will tell you future!! Please Enter your name..");
             name = Console.ReadLine();
-            Console.WriteLine("{0} --- That's the best your parent can come up with??", name);
-            Console.WriteLine("never mind, let's get started ..");
-
-            Console.WriteLine(this.RunGematriaCalculation(name));
-
+            Console.WriteLine("!!!!!!Lets get started!!!!!!!");
+            this.RunGematrriaCalculation(name);
         }
-
-        public int RunGematriaCalculation(string thename)
+        public void RunGematrriaCalculation(string name)
         {
-            int GematriaValue = 0;
-            int total = 0;
-
-
-            foreach (char letter in thename)
+            int sum = 0;
+            foreach (char i in name)
             {
-                GematriaValue = this.ReturnGematriaValueofLetter(letter.ToString());
-                total += GematriaValue;
-
+                sum += this.ReturnGematriaValueOfLetter(i);
             }
-            return total;
+            Console.WriteLine("aggregate function " + sum);
+
+
+
+            this.SecondSum(sum);
+
 
         }
-        public int ReturnGematriaValueofLetter(string letter)
+        public int ReturnGematriaValueOfLetter(char i)
         {
-            return LetterValue[letter.ToUpper()];
+            string temp;
+            temp = i.ToString().ToUpper();
+            Dictionary<String, int> Gematria = new Dictionary<String, int>()
+            {
+            {"A",1},
+            {"B", 2},
+            {"C",3},
+            {"D",4},
+            {"E", 5},
+            {"F",6},
+            {"G",7},
+            {"H",8},
+            {"I",9},
+            {"J",1},
+            {"K", 2},
+            {"L",3},
+            {"M",4},
+            {"N", 5},
+            {"O",6},
+            {"P",7},
+            {"Q", 8},
+            {"R",9},
+            {"S",1},
+            {"T", 2},
+            {"U",3},
+            {"V",4},
+            {"W", 5},
+            {"X",6},
+            {"Y",7},
+            {"Z", 8}
+
+              };
+            Console.WriteLine(" key value is  " + temp + " is:");
+            Console.WriteLine(Gematria[temp]);
+            return Gematria[temp];
+
+        }
+
+        public void SecondSum(int temp2)
+        {
+
+
+
+            int sum = 0;
+            while (temp2 > 0)
+            {
+                sum = sum + temp2 % 10;
+                temp2 = temp2 / 10;
+            }
+            Console.WriteLine(" Fianl sum  : " + sum);
         }
     }
-    class playground
-    {
-        public void Run()
-        {
-            Dictionary<string, int> LetterValue = new Dictionary<string, int>()
-
-{ {"A",1 },
-{"B",2 },
-{"C",3},
-{"D",4},
-{"E",5},
-{"F",6},
-{"G",7},
-{"H",8 },
-{"I",9},
-{"J",1},
-{"K",2 },
-{"L",3},
-{"M",4},
-{"N",5},
-{"O",6},
-{"P",7 },
-{"Q",8},
-{"R",9},
-{"S",1 },
-{"T",2},
-{"U",3 },
-{"V",4},
-{"W",5},
-{"X",6},
-{"Y",7},
-{"Z",8}
-
-};
-            int temp = LetterValue["B"];
-            Console.WriteLine(temp);
-        }
-
-    }
-
-
 }
